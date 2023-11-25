45106131bab680e81c3d7849e5b31fa518cda4b2
anderk222 <anderk222@gmail.com>
2023-11-25 15:16:41 -0500

    init server project using Spring Boot

---
+++ fnbr-form-serve/src/main/java/fnbr/fnbrform/FnbrFormApplication.java
@@ -0,0 +1,13 @@
+package fnbr.fnbrform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnbrFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnbrFormApplication.class, args);
+	}
+
+}
---
+++ fnbr-form-serve/src/test/java/fnbr/fnbrform/FnbrFormApplicationTests.java
@@ -0,0 +1,13 @@
+package fnbr.fnbrform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnbrFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
